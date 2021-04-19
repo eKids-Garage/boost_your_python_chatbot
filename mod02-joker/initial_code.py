8e715faa55dc1312a1834027107da47c746c51f0
Dmitry Orekhov <dmitry_orekhov@epam.com>
2021-04-19 08:36:50 +0300

    Functions, final

--- mod02-joker/initial_code.py
+++ mod02-joker/initial_code.py
@@ -1,1 +1,22 @@
-# Initial Code
+#func What we should get after the Module 1
+chatbot_name = "Garik"
+
+user_name = input("Hello! What's you name? ")
+
+#1
+phrase = input(chatbot_name + ": What do you think? ")
+print("Yes, " + user_name + ", " + phrase)
+
+#2
+phrase = input(chatbot_name + ": What do you think? ")
+print("Yes, " + user_name + ", " + phrase)
+
+#3
+phrase = input(chatbot_name + ": What do you think? ")
+print("Yes, " + user_name + ", " + phrase)
+
+#4
+phrase = input(chatbot_name + ": What do you think? ")
+print("Yes, " + user_name + ", " + phrase)
+
+# ... and so on and so forth
